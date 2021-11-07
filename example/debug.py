aade1b6e0e34ebd88fe9ba734b12e71b9a88d534
Mattéo <40558258+Matteo0810@users.noreply.github.com>
2021-11-07 12:09:33 +0100

    Delete example directory

--- example/debug.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-import sys
-path = str(Path(Path(__file__).parent.absolute()).parent.absolute())
-sys.path.insert(0, path)
-
-def debug(message, success):
-    print(f'[DEBUG TEST] {message} : {("❌", "✅")[success]}')
